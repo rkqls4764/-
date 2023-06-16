7299711af9e5e95c2a8c5fa18f60fe5f99ba7781
gaabeen <62369558+rkqls4764@users.noreply.github.com>
2023-06-16 22:57:36 +0900

    Upload Source

---
+++ PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y > ScoreManager.heightScore)
+        {
+            ScoreManager.heightScore = (int)transform.position.y;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Apple은 점수 30 증가
+        if (collision.CompareTag("Apple"))
+        {
+            ScoreManager.appleScore += 30;
+            Destroy(collision.gameObject);
+        }
+        //WormApple은 점수 10 감소
+        else if (collision.CompareTag("WormApple"))
+        {
+            ScoreManager.appleScore -= 10;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int totalScore;
+    public static int heightScore;
+    public static int appleScore;
+
+    void Awake()
+    {
+        heightScore = 0;
+        appleScore = 0;
+        totalScore = 0;
+    }
+
+    void Update()
+    {
+        totalScore = heightScore + appleScore;
+    }
+}
