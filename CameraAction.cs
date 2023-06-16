b764c5265fdcfd8697105d1bbefed10b7f685f0c
gaabeen <62369558+rkqls4764@users.noreply.github.com>
2023-06-16 23:01:10 +0900

    Upload Source

---
+++ CameraAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAction : MonoBehaviour
+{
+    public GameObject target;
+    public float moveSpeed;
+    public float distanceY = 1.0f;
+
+    Vector3 targetPos;
+
+    void Update()
+    {
+        targetPos.Set(this.transform.position.x, target.transform.position.y + distanceY, this.transform.position.z);
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+}
